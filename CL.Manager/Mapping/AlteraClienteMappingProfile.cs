4c0e9c12b97825b70b551ae2e74bfccbd291c078
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-10-26 23:15:55 -0300

    Implementação do mapping para o input de novo cliente e alterações no cliente

---
+++ CL.Manager/Mapping/AlteraClienteMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CL.Core.Domain;
+using CL.Core.InputModel;
+
+namespace CL.Manager.Mapping;
+
+public class AlteraClienteMappingProfile : Profile
+{
+    public AlteraClienteMappingProfile()
+    {
+        CreateMap<AlteraCliente, Cliente>().ForMember(clienteAlterado => clienteAlterado.Nascimento, op => op.MapFrom(cliente => cliente.Nascimento.Date))
+            .ForMember(clienteAlterado => clienteAlterado.UltimaAtualizacao, op => op.MapFrom(cliente => DateTime.Now));
+    }
+}
---
+++ CL.Manager/Mapping/NovoClienteMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using CL.Core.Domain;
+using CL.Core.InputModel;
+
+namespace CL.Manager.Mapping;
+
+public class NovoClienteMappingProfile : Profile
+{
+	public NovoClienteMappingProfile()
+	{
+		CreateMap<NovoCliente, Cliente>().ForMember(novoCliente => novoCliente.Nascimento, op => op.MapFrom(cliente => cliente.Nascimento.Date))
+			.ForMember(novoCliente => novoCliente.Criacao, op => op.MapFrom(cliente => DateTime.Now));
+	}
+}
