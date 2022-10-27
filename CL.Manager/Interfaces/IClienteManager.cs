2084909702f508fbe988e3a1176835603aaa40dc
Matheus Freitas <freitas.matheus.profissional@outlook.com>
2022-10-26 23:14:27 -0300

    Uso de interfaces para evitar o acoplamento.

---
+++ CL.Manager/Interfaces/IClienteManager.cs
@@ -0,0 +1,13 @@
+﻿using CL.Core.Domain;
+using CL.Core.InputModel;
+
+namespace CL.Manager.Interfaces;
+
+public interface IClienteManager
+{
+    Task<Cliente> DeleteClienteAsync(int id);
+    Task<Cliente> GetClienteAsync(int id);
+    Task<IEnumerable<Cliente>> GetClientesAsync();
+    Task<Cliente> InsertClienteAsync(NovoCliente novoCliente);
+    Task<Cliente> UpdateClienteAsync(AlteraCliente clienteAlterado);
+}
---
+++ CL.Manager/Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using CL.Core.Domain;
+
+namespace CL.Manager.Interfaces;
+
+public interface IClienteRepository
+{
+    Task<Cliente> DeleteClienteAsync(int id);
+    Task<Cliente> GetClienteAsync(int id);
+    Task<IEnumerable<Cliente>> GetClientesAsync();
+    Task<Cliente> InsertClienteAsync(Cliente cliente);
+    Task<Cliente> UpdateClienteAsync(Cliente clienteAlterado);
+}
